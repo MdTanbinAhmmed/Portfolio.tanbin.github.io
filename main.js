2bfff8fcae27465a5a50bdfcd2c2b2ee0212117b
Md Tanbin Ahmmed <buyeruse4@gmail.com>
2024-02-01 20:39:54 +0600

    all

---
+++ main.js
@@ -0,0 +1,14 @@
+  // const typed = new Typed('.typed', {
+  //   strings: ['Front-End Web Developer', 'Engineer', 'Programmer'],
+  //   typeSpeed: 80,
+  //   backspeed: 150,
+  //   backDelay: 1500,
+  //   loop: true,
+  //   loopCount: 1100000,
+  //   cursorChar: '.'
+  // });
+
+
+document.getElementById("demo").innerHTML = "Date : " + Date();
+
+ 
